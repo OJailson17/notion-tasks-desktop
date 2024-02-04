646a7bb1b6722f0f7b418f8cb6f50909c44f16fa
OJailson17 <JaylsonO17@gmail.com>
2024-02-04 02:01:29 -0300

    chore: add optional tray icon path depending on the env

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -1,7 +1,11 @@
 const { Tray, nativeImage } = require('electron');
 
 function createTray() {
-	const icon = nativeImage.createFromPath('./resources/assets/icon.png');
+	const iconPath =
+		process.env.NODE_ENV && process.env.NODE_ENV === 'dev'
+			? 'assets/icon.png'
+			: './resources/assets/icon.png';
+	const icon = nativeImage.createFromPath(iconPath);
 	const tray = new Tray(icon);
 
 	tray.setToolTip('Notion Tasks');
